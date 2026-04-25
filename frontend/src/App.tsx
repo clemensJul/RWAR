a6c6a9fa49313a98ddfcb55f1971b029718f82b1
Clemens Blum <clemens.j.blum@gmail.com>
2026-04-25 10:59:55 +0200

    init: frontend + project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import Map from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+
+function App() {
+  return (
+    <>
+      <Map
+        mapboxAccessToken="<Mapbox access token>"
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </>
+  )
+}
+
+export default App
