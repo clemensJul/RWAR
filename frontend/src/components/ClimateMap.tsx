b3d5442cbc98fca84eca87698b2a3846f62b4977
Clemens Blum <clemens.j.blum@gmail.com>
2026-04-25 14:07:24 +0200

    init: fix backend

--- frontend/src/components/ClimateMap.tsx
+++ frontend/src/components/ClimateMap.tsx
@@ -4,12 +4,11 @@ function ClimateMap() {
     const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
     return (
         <>
-            <h2>Hi</h2>
             <Map
                 mapboxAccessToken={MAPBOX_TOKEN}
                 initialViewState={{ longitude: -122.4, latitude: 37.8, zoom: 14 }}
-                style={{ width: '100%', height: '100%' }} // Map fills the Tailwind wrapper
-                mapStyle="mapbox://styles/mapbox/dark-v11"
+                style={{ width: '100%', height: '100%' }}
+                mapStyle="mapbox://styles/mapbox/outdoors-v12"
             />
         </>
     );
